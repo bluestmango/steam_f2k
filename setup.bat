615dfdeada3ca1bcdecccf30376222568be2bab3
bluestmango <darthspider@duck.com>
2025-06-07 11:53:07 -0230

    fix windows setup

--- setup.bat
+++ setup.bat
@@ -1,11 +1,10 @@
 @echo OFF
-schtasks /create /tn steamf2k /xml config\schedule.xml
+schtasks /create /tn steamf2k /xml "%~dp0config/schedule.xml"
 if %ERRORLEVEL% NEQ 0 (
 	echo Setup error. Make sure to run as administrator
 	pause
 	exit /b %ERRORLEVEL%
 )
-schtasks /run /tn steamf2k
 echo Setup will now launch the Windows Task Scheduler
 pause
 taskschd.msc
